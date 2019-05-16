f10f0869e80cecc8b8701c5aaf8358d98798d1fe
yussuki <uehara_thiago@hotmail.com>
2019-05-16 16:52:00 -0300

    Implementada as funções Program, Func, ParamList, ParamDec e StatList

--- F01_Anderson_Andre_GabrielEiji_Thiago/Compiler.java
+++ F01_Anderson_Andre_GabrielEiji_Thiago/Compiler.java
@@ -4,9 +4,75 @@ import AST.Program;
 
 public class Compiler {
 
+	char token;
+
 	public Program compile(char[] input, PrintWriter printWriter) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	public void Program() {
+		Func();
+		while(token != EOF)
+			Func();
+	}
+
+	public void Func() {
+		if(token == 'F') {
+			Id();
+			if(token = '(') {
+				nextToken();
+				ParamList();
+				
+				if(token == ')') {
+					nextToken();
+				}
+				else error();
+			}
+			if(token == '-') {
+				nextToken();
+				if(token == '>')
+				{
+					nextToken();
+					Type();
+				}
+				else error();
+			}
+
+			StateList();
+		} else error();
+	}
+
+	public void ParamList() {
+		ParamDec();
+
+		while(token != ',')
+			ParamDec();
+	}
+
+	public void ParamDec() {
+		Id();
+		if(token == ':')
+		{
+			nextToken();
+			Type();
+		}
+		else error();
+	}
+
+	public void Type() {
+		if(token != 'I' && token != 'B' && token != 'S')
+			error();
+	}
+	 
+	public void StatList() {
+		if(token == '{') {
+			while(token != '}')
+				Stat();
+		}
+	}
+
+	public void Stat() {
+		
+	}
 }
